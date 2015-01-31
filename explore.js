7a68c65633c7a2d62a05ff6050f21c7440d340e7
greenteawarrior <emily.wang@students.olin.edu>
2015-01-31 04:21:26 +0000

    attempting bar charts tutorial, part II (chart isn't showing up on webpage - yet)

--- explore.js
+++ explore.js
@@ -2,25 +2,48 @@
 // Emily Wang
 // January 26, 2015
 
-// Code snippets from the introduction on http://d3js.org/
+// bar chart tutorial, part 2
+// http://bost.ocks.org/mike/bar/2/
 
-d3.json("CitiBikeFebruary2014_100rows.json", function(json) {
-    citibikeData = json;
-});
-
-var data = [4, 8, 15, 16, 23, 42];
-
-// bar chart tutorial, part 1
-// http://bost.ocks.org/mike/bar/
+var width = 420;
+var barHeight = 20;
 
 // mapping from data space (domain) to display space (range)
-scaling = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+var xscaling = d3.scale.linear()
+    .range([0, width]);
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return scaling(d) + "px"; })
-    .text(function(d) { return d; });
+var chart = d3.select(".chart")
+    .attr("width", width)
+
+// beware globals for now!
+// todo: make variable scope more robust
+d3.json("CitiBikeFebruary2014_100rows.json", function(error, json) {
+    citibikeData = json;
+    console.log("loaded CitiBikeFebruary2014_100rows data!");
+    tripdurationVals = d3.values(citibikeData["tripduration"]);
+    console.log(d3.max(tripdurationVals));
+    xscaling.domain([0, d3.max(tripdurationVals)]);
+
+    chart.attr("height", barHeight * tripdurationVals.length);
+
+    var bar = chart.selectAll("g")
+        .data(citibikeData)
+      .enter().append("g")
+        .attr("transform", function (d, i) { return "translate(0," + i * barHeight + ")"; })
+
+    bar.append("rect")
+        .attr("width", function(d) { return xscaling(d.value); } )
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function(d) { return x(d.value) - 3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.value; });
+
+});
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
